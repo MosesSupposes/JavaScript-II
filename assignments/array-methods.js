66abd598f253e9b90ca39ec442362e69dcc70095
Moses Samuel <mosamuel98@gmail.com>
2019-04-02 12:31:44 -0700

    solve array challenge 3

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -60,7 +60,7 @@ function challenge1(list) {
     list.forEach(runner => fullName.push(`${runner.first_name} ${runner.last_name}`))
     return fullName
 }
-console.log(challenge1(runners));
+console.log('Challenge 1\n', challenge1(runners));
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
@@ -69,17 +69,24 @@ function challenge2(list){
     return list.map(firstNameToUpper);  
 }
 
-console.log(challenge2(runners)); 
+console.log('Challenge 2\n', challenge2(runners)); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+function challenge3() {
+    return runners.filter(runner => runner.shirt_size === 'L')
+}
 
-console.log(largeShirts);
+console.log('Challenge 3\n', challenge3());
+
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+
+
+
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
